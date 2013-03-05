1966fc378e51fc71962d7133ce4781ba6a031084
little-eyes <jilong.liao@gmail.com>
2013-03-05 14:54:41 -0500

    app trigger class

--- src/com/android/recapture/lib/ApplicationTriggers.java
+++ src/com/android/recapture/lib/ApplicationTriggers.java
@@ -1,6 +1,67 @@
 package com.android.recapture.lib;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+
 public class ApplicationTriggers {
 	
-
+	/*
+	 * The complete class path to trigger an app. For example,
+	 * the full class path for facebook is com.android.facebook
+	 * TODO: figure out all possible apps' class path and test.
+	 * */
+	public final static String APP_FACEBOOK_PACKAGE_NAME = "com.facebook.katana";
+	public final static String APP_FACEBOOK_CLASS_NAME = null;
+	
+	public final static String APP_GMAIL_PACKAGE_NAME = "com.android.gmail";
+	public final static String APP_GMAIL_CLASS_NAME = null;
+	
+	public final static String APP_TWITTER_PACKAGE_NAME = "com.android.twitter";
+	public final static String APP_TWITTER_CLASS_NAME = null;
+	
+	public final static String APP_PHONE_PACKAGE_NAME = "com.android.phone";
+	public final static String APP_PHONE_CLASS_NAME = null;
+	
+	public final static String APP_SMS_PACKAGE_NAME = "com.android.sms";
+	public final static String APP_SMS_CLASS_NAME = null;
+	
+	public final static String APP_MAPS_PACKAGE_NAME = "com.android.maps";
+	public final static String APP_MAPS_CLASS_NAME = null;
+	
+	/*
+	 * Variable stores which application this trigger is for.
+	 * */
+	private static String _ApplicationPackageName_;
+	private static String _ApplicationClassName_;
+	private static Context _AppEnvironment_;
+	
+	public ApplicationTriggers(String pkgName, String className, Context context) {
+		_ApplicationPackageName_ = pkgName;
+		_ApplicationClassName_ = className;
+		_AppEnvironment_ = context;
+	}
+	
+	public ApplicationTriggers(String pkgName, Context context) {
+		_ApplicationPackageName_ = pkgName;
+		_ApplicationClassName_ = null;
+		_AppEnvironment_ = context;
+	}
+	
+	/*
+	 * Launch the application by package name only or package name + main activity class.
+	 * */
+	boolean triggerApplication() {
+		if (_ApplicationClassName_ != null) {
+			Intent intent = new Intent();
+			intent.setComponent(new ComponentName(_ApplicationPackageName_, _ApplicationClassName_));
+			_AppEnvironment_.startActivity(intent);
+			return true;
+		}
+		else {
+			Intent intent = _AppEnvironment_.getPackageManager().getLaunchIntentForPackage(_ApplicationPackageName_);
+			_AppEnvironment_.startActivity(intent);
+			return true;
+		}
+	}
 }
